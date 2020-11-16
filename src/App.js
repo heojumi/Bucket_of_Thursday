7cfdb61dae3471418858a4413b9b15bfa4c52f1a
geun9716 <crintop@gmail.com>
2020-11-16 18:22:09 +0900

    개발환경 설정 2차

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
+import {Home, About, Main} from './pages';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render(){
+    return(
+      <div>
+        <Route exact path="/" component={Home}/>
+        <Route path="/main" component={Main}/>
+        <Switch>
+          <Route path="/about/:name" component={About}/>
+          <Route path="/about" component={About}/>
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Menu} from '../components';
+const Home = () => {
+    return(
+        <div>
+            <Menu/>
+            <h2>Home</h2>
+        </div>
+    );
+};
+
+export default Home;
