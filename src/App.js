c5aeb1166961dc34d1802f4da71ef7b990983920
geun9716 <crintop@gmail.com>
2020-11-19 17:03:05 +0900

    Add Menu

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React, {Component} from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import {Home, About, Main} from './pages';
-
+import {Menu} from './components';
 class App extends Component {
   render(){
     return(
       <div>
+        <Menu/>
         <Route exact path="/" component={Home}/>
         <Route path="/main" component={Main}/>
         <Switch>
