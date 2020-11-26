db0c6c1a5e1781c130cb997caeb2ed3bd1fb8fd6
geun9716 <crintop@gmail.com>
2020-11-26 17:11:21 +0900

    클래스형 컴포넌트로 변경 및 main예시

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
+const mariadb = require('mariadb');
+const pool = mariadb.createPool({
+  host : '127.0.0.1',
+  user : 'root',
+  password : 'bucket',
+  connectionLimit : 5,
+  database : 'bucket'
+});
+
+const con = pool.getConnection();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.send('this is main');
 });
+
+// router.get('/:id', function(req, res)){
+//   let id = req.route.params;
+//   con.query("Select * from user_tb;")
+//   .then((rows)=>{
+//     if(rows.length){
+//       res.send({lists: rows});
+//     }
+//   })
+// }
 
 module.exports = router;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,27 +2,63 @@ import React from 'react';
 import {Menu} from '../components';
 import './Home.css';
 
+lists =[
+    {
+        bid : 1,
+        title : 'bucket 1',
+        status : 1,
+    },
+    {
+        bid : 1,
+        title : 'bucket 1',
+        status : 1,
+    },
+    {
+        bid : 1,
+        title : 'bucket 1',
+        status : 1,
+    }
+]
 
-const Home = () => {
-    return(
-        <div class="login-page">
-  <div class="form">
-    <form class="register-form">
-      <input type="text" placeholder="name"/>
-      <input type="password" placeholder="password"/>
-      <input type="text" placeholder="email address"/>
-      <button>create</button>
-      <p class="message">Already registered? <a href="#">Sign In</a></p>
-    </form>
-    <form class="login-form">
-      <input type="text" placeholder="username"/>
-      <input type="password" placeholder="password"/>
-      <button>login</button>
-      <p class="message">Not registered? <a href="#">Create an account</a></p>
-    </form>
-  </div>
-</div>
-    );
+class Home extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state ={
+            lists : [],
+        }
+    }
+    //Loading 되기 전에 발생하는 이벤트 cycle
+    componentDidmount = () => {
+
+    }
+
+    postLogin = () => {
+        console.log('post login');
+        alert('login');
+    }
+
+    render(){
+        return(
+            <div class="login-page">
+                <div class="form">
+                    <form class="register-form">
+                        <input type="text" placeholder="name"/>
+                        <input type="password" placeholder="password"/>
+                        <input type="text" placeholder="email address"/>
+                        <button>create</button>
+                        <p class="message">Already registered? <a href="#">Sign In</a></p>
+                    </form>
+                    <form class="login-form">
+                        <input type="text" placeholder="username"/>
+                        <input type="password" placeholder="password"/>
+                        <button onClick={this.postLogin}>login</button>
+                        <p class="message">Not registered? <a href="#">Create an account</a></p>
+                    </form>
+                </div>
+            </div>
+        );
+    }
 };
 
 export default Home;
